259cb429fcd13615f8fb691ce55a395ec8a27cc7
karen-bukowsky <karen.valadez.n@gmail.com>
2023-01-29 18:22:31 -0600

    listo

---
+++ clase nomina.js
@@ -0,0 +1,27 @@
+class Retardos {
+    horaEntrada;
+    descuento;
+    constructor(hE, desc){
+        this.horaEntrada= hE;
+        this.descuento=desc;
+    }
+
+}
+
+class TrabajadoresAreas{
+    puesto;
+    experiencia;
+    antiguedad;
+    profesion;
+    periodoContrato;
+    horario;
+    constructor(pues,exp,antig,prof,perCon,hora){
+        this.puesto= pues;
+        this.experiencia= exp;
+        this.antiguedad=antig;
+        this.profesion=prof;
+        this.periodoContrato=perCon;
+        this.horario=hora;
+    
+    }
+}
