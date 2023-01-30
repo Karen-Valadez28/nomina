606716a88137670cdc9f38dd3081a0fb8d495e68
Jorge Enrique Servin Alvarez <j25450410@gmail.com>
2023-01-29 19:30:03 -0600

    Primer movimiento

--- clase nomina.js
+++ clase nomina.js
@@ -1,3 +1,17 @@
+class Faltas{
+    FechaFalta;
+    Motivo;
+    Justificacion;
+
+    constructor(FechaFalta, Motivo, Justificacion){
+    
+    
+        this.FechaFalta= Fal;
+        this.Motivo= Mo;
+        this.Justificacion= Just;
+    }
+}
+
 class Retardos {
     horaEntrada;
     descuento;
@@ -23,5 +37,19 @@ class TrabajadoresAreas{
         this.periodoContrato=perCon;
         this.horario=hora;
     
+    }
+}
+class Banco{
+    ComprobanteDomicilio;
+    NumCuenta;
+    TipoBanco ;
+    Beneficiarios;
+
+    constructor(ComprobanteDomicilio, NumCuenta,TipoBanco,Beneficiarios){
+
+        this.ComprobanteDomicilio;
+        this.NumCuenta= NuC;
+        this.TipoBanco= TiB;
+        this.Beneficiarios= Ben;
     }
 }
