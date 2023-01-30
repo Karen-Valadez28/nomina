c7becee309013a29aad6b6e4b288935e9867db3d
Jorge Enrique Servin Alvarez <j25450410@gmail.com>
2023-01-29 20:00:25 -0600

    primer comentario

--- clase nomina.js
+++ clase nomina.js
@@ -43,3 +43,30 @@ class sueldo{
 class Administradores{
     
 }
+class Faltas{
+    FechaFalta;
+    Motivo;
+    Justificacion;
+
+    constructor(FechaFalta, Motivo, Justificacion){
+    
+    
+        this.FechaFalta= Fal;
+        this.Motivo= Mo;
+        this.Justificacion= Just;
+    }
+}
+class Banco{
+    ComprobanteDomicilio;
+    NumCuenta;
+    TipoBanco ;
+    Beneficiarios;
+
+    constructor(ComprobanteDomicilio, NumCuenta,TipoBanco,Beneficiarios){
+
+        this.ComprobanteDomicilio;
+        this.NumCuenta= NuC;
+        this.TipoBanco= TiB;
+        this.Beneficiarios= Ben;
+    }
+}
